1a07da8fa79943d0f25dc9d34cf7fd921058d686
Vlad-Andrei Medintu <80452570+Vladutzu27@users.noreply.github.com>
2022-01-16 10:59:59 +0200

    Add files via upload

---
+++ shortcut to permalink.py
@@ -0,0 +1,4 @@
+import webbrowser;
+print("this is a shortcut to my Windows xp project.");
+print("redirecting...");
+webbrowser.open('https://play.scoolcode.com/60c3068426461d7ff64ae028?variant=staging');
